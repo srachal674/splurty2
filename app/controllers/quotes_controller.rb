1612abdb08f8bea793eca12438a28535a66a52b2
Shannon Rachal <srachal674@gmail.com>
2019-10-14 06:01:29 +0000

    Trying to add all files

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,29 @@
+class QuotesController < ApplicationController
+  def index
+    @quote = Quote.order("Random()").first
+  end
+  
+  def create
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end 
+    redirect_to root_path
+  end
+
+  def show
+    @quote = Quote.where(:id => params[:id]).first
+      if @quote.blank?
+    render :text => "Not Found", :status => :not_found
+    end
+  end
+
+  def about
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+end
